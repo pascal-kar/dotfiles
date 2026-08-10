e227969cefe3a8d33c1c40a6ca314e09ffa5c623
pascal-kar <163018351+pascal-kar@users.noreply.github.com>
2026-08-10 22:08:47 +0200

    feature: added basic packages in nix config
    Added configuration for various programs and system packages.

--- configuration.nix
+++ configuration.nix
@@ -91,3 +91,63 @@
     ];
     shell = pkgs.fish;
   };
+
+  # Enable automatic login for the user.
+  # services.xserver.displayManager.autoLogin.enable = false;
+
+  # Install firefox.
+  programs = {
+    firefox.enable = true;
+    fish.enable = true;
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    wget
+    git
+    vim
+    ripgrep
+    fish
+    discord
+    emacs
+    keepassxc
+    stow
+
+    # language server
+    gopls
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "26.05"; # Did you read the comment?
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+}
